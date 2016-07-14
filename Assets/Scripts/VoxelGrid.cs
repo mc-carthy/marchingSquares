4f24cd435d2ec33de993dd2589e9a29a5daf3ef7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-14 19:57:55 +0100

    Create unit voxel grid

---
+++ Assets/Scripts/VoxelGrid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoxelGrid : MonoBehaviour {
+
+	public int resolution;
+	public GameObject voxelPrefab;
+
+	private bool[] voxels;
+	private float voxelSize;
+
+	private void Awake () {
+		voxelSize = 1f / resolution;
+		voxels = new bool[resolution * resolution];
+
+		for (int i = 0, y = 0; y < resolution; y++) {
+			for (int x = 0; x < resolution; x++, i++) {
+				CreateVoxel (i, x, y);
+			}
+		}
+	}
+
+	private void CreateVoxel (int i, int x, int y) {
+		GameObject o = (GameObject)Instantiate (voxelPrefab);
+		o.transform.SetParent (transform);
+		o.transform.localPosition = new Vector3 ((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+		o.transform.localScale = Vector3.one * voxelSize;
+	}
+}
